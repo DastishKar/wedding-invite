d79df8bbeb85156075fcbb5f7d20dfe147833c10
Dastan Karbayev <karbayevd@gmail.com>
2026-06-18 17:31:39 +0500

    second commit

--- src/components/PhotoSlide.js
+++ src/components/PhotoSlide.js
@@ -1,5 +1,6 @@
 import MotionSection from './MotionSection';
 import SectionPhoto from './SectionPhoto';
+import { CardOrnaments } from './Ornament';
 
 function PhotoSlide({ children, photoSrc, textZone = 'center', className = '', stagger = true, ...props }) {
   const isPlain = !photoSrc;
@@ -9,6 +10,7 @@ function PhotoSlide({ children, photoSrc, textZone = 'center', className = '', s
       className={`invite-slide ${isPlain ? 'invite-slide--plain' : ''} invite-slide--${textZone} ${className}`.trim()}
     >
       {photoSrc ? <SectionPhoto src={photoSrc} /> : null}
+      <CardOrnaments />
       <MotionSection className="invite-slide__text" stagger={stagger} {...props}>
         <div className="invite-slide__text-inner">{children}</div>
       </MotionSection>
